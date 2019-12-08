858ee0c78806bd9c61c99505cd9bed4b56e36214
Pishoy <pishoyabdo@gmail.com>
2019-12-08 15:40:08 +0200

    Create package.py

---
+++ html/package.py
@@ -0,0 +1,14 @@
+
+[source]
+name = "fftoken_io"
+description = "fftoken website"
+threebot = "zerobot"
+version = "1.0.0"
+
+[actor]
+namespace = "fftoken_act_ns" 
+
+[[bcdbs]]
+namespace = "fftoken_ns"
+type = "redis"
+instance = "default"
