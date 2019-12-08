79ee37817b1956e845650ce2dd53b392c8d2082e
Pishoy <pishoyabdo@gmail.com>
2019-12-08 15:42:11 +0200

    Update package.py

--- html/package.py
+++ html/package.py
@@ -1,14 +1,28 @@
+from Jumpscale import j
 
-[source]
-name = "fftoken_io"
-description = "fftoken website"
-threebot = "zerobot"
-version = "1.0.0"
+class Package(j.baseclasses.threebot_package):
+    """
+    kosmos -p
+    cl = j.servers.threebot.local_start_default()
+    j.threebot.packages.zerobot.packagemanager.actors.package_manager.package_add(git_url="https://github.com/Pishoy/www_incubaid")
+    """
 
-[actor]
-namespace = "fftoken_act_ns" 
+    def _init(self, **kwargs):
+        self.branch = kwargs["package"].branch or "master"
+        self.enertia_io = "https://github.com/Pishoy/www_incubaid"
 
-[[bcdbs]]
-namespace = "fftoken_ns"
-type = "redis"
-instance = "default"
+    def prepare(self):
+        website = self.openresty.get_from_port(443)
+        locations = website.locations.get("fftoken")
+        static_location = locations.locations_static.new()
+        static_location.name = "static"
+        static_location.path_url = "html"
+        path = j.clients.git.getContentPathFromURLorPath(self.enertia_io, branch=self.branch, pull=True)
+        html_pah = "{}/html".format(path)
+        static_location.path_location = html_path
+        static_location.use_jumpscale_weblibs = True # if set, will copy weblibs and serve it from /static/weblibs directly
+        locations.configure()
+        website.configure()
+
+    def start(self):
+        self.prepare()
