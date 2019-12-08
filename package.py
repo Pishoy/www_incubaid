e670a06cd9ed04a4070af93d732577031fba3021
Bishoy <abdelmab@codescalers.com>
2019-12-08 18:28:05 +0200

    add http/s

--- package.py
+++ package.py
@@ -12,17 +12,18 @@ class Package(j.baseclasses.threebot_package):
         self.incubaid_com = "https://github.com/Pishoy/www_incubaid"
 
     def prepare(self):
-        website = self.openresty.get_from_port(443)
-        locations = website.locations.get("incubaid_com")
-        static_location = locations.locations_static.new()
-        static_location.name = "incubaid_static"
-        static_location.path_url = "/incubaid"
-        path = j.clients.git.getContentPathFromURLorPath(self.incubaid_com, branch=self.branch, pull=True)
-        html_path = "{0}/html".format(path)
-        static_location.path_location = html_path
-        static_location.use_jumpscale_weblibs = True # if set, will copy weblibs and serve it from /static/weblibs directly
-        locations.configure()
-        website.configure()
+        for port in (443, 80):
+            website = self.openresty.get_from_port(port)
+            locations = website.locations.get("incubaid_com")
+            static_location = locations.locations_static.new()
+            static_location.name = "incubaid_static"
+            static_location.path_url = "/incubaid"
+            path = j.clients.git.getContentPathFromURLorPath(self.incubaid_com, branch=self.branch, pull=True)
+            html_path = "{0}/html".format(path)
+            static_location.path_location = html_path
+            static_location.use_jumpscale_weblibs = True # if set, will copy weblibs and serve it from /static/weblibs directly
+            locations.configure()
+            website.configure()
 
     def start(self):
         self.prepare()
